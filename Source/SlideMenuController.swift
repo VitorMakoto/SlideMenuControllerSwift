3ce054dc0d22efbf211332b2bafef547c9b88a58
Vitor Makoto <makoto@work.co>
2016-11-29 14:58:33 -0200

    wip

--- Source/SlideMenuController.swift
+++ Source/SlideMenuController.swift
@@ -18,7 +18,7 @@ import UIKit
     optional func rightDidClose()
 }
 
-public class SlideMenuOptions: NSObject {
+public class SlideMenuOptions {
     public static var leftViewWidth: CGFloat = 270.0
     public static var leftBezelWidth: CGFloat? = 16.0
     public static var contentViewScale: CGFloat = 1.0
